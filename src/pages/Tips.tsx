35e495f654d41360e1f789d71315a33a29d836e1
leobbianchi <leobbianchi@gmail.com>
2024-11-20 09:02:41 -0300

    ajustes na pagina de dicas

--- src/pages/Tips.tsx
+++ src/pages/Tips.tsx
@@ -15,29 +15,29 @@ const hotelData = [
     distance: '10 minutos de carro',
     address: 'Viale Italia, 24, 23864 Malgrate LC',
     mapSrc: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d40005.41682743714!2d9.363209786331646!3d45.83837164178876!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47841d163096031f%3A0xef1d970295d63eb3!2sHotel%20Griso%20Collection!5e0!3m2!1spt-BR!2sbr!4v1731665482487!5m2!1spt-BR!2sbr',
-    link: 'https://www.booking.com/hotel/it/griso.pt-pt.html'
+    link: 'https://www.griso.info/en',
   },
   {
     name: 'Hotel Alberi',
     distance: '10 minutos de carro',
     address: 'Via Lungo Lario Isonzo 4, 23900 Lecco',
     mapSrc: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4718.765487028866!2d9.383171335491832!3d45.85304508591444!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47841d245d7f421d%3A0x34d60945f7c3abaa!2sHotel%20Alberi%20Lecco!5e0!3m2!1spt-BR!2sbr!4v1731666095747!5m2!1spt-BR!2sbr',
-    link: 'https://www.booking.com/hotel/it/alberi.pt-pt.html'
+    link: 'https://www.hotelalberi.it/en',
   },
   {
     name: 'Locanda Papà Nenè',
     distance: '10 minutos de carro',
     address: '24 Via Pinamonte, 23875 Osnago',
     mapSrc: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2787.757428203178!2d9.392510711865237!3d45.67576977095757!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4786ae2cc3cb53d7%3A0x6a29127ff43d53d9!2sOsteria%20Roncate!5e0!3m2!1spt-BR!2sbr!4v1731666388429!5m2!1spt-BR!2sbr',
-    link: 'https://www.booking.com/hotel/it/locanda-papa-nene.pt-pt.html'
+    link: 'https://osterialocanda.wixsite.com/english',
   },
   {
     name: 'Melas Hotel',
     distance: '5 minutos de carro',
     address: 'Via Bergamo 37, 23807 Merate',
     mapSrc: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2787.1059068768004!2d9.421996799999999!3d45.6888466!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4786ae6a91f7e14b%3A0xc4cdbadb0bf79ac3!2sMelas%20Hotel!5e0!3m2!1spt-BR!2sbr!4v1731671706797!5m2!1spt-BR!2sbr',
-    link: 'https://www.booking.com/hotel/it/melas.pt-br.html'
-  }
+    link: 'https://www.melashotel.it',
+  },
 ];
 
 const Tips: React.FC = () => {
@@ -57,8 +57,7 @@ const Tips: React.FC = () => {
       </div>
 
       <p className="intro-text">
-        Merate, uma encantadora cidade localizada na região da Lombardia, no norte da Itália, é conhecida por sua paisagem pitoresca, com colinas verdejantes, arquitetura histórica e uma atmosfera acolhedora e tranquila. A cidade é rica em tradição e oferece um ambiente perfeito para um momento tão especial.
-        Embora pequena, Merate tem uma grande importância histórica e cultural, com belas igrejas, praças charmosas e uma proximidade com algumas das cidades mais icônicas da Itália, como Milão (41 min / 39,4 km) e Como (42 min / 35,8 km).
+        Merate, uma encantadora cidade localizada na região da Lombardia, no norte da Itália, é conhecida por sua paisagem pitoresca, com colinas verdejantes, arquitetura histórica e uma atmosfera acolhedora e tranquila.
       </p>
 
       <div className="map-container">
@@ -73,11 +72,16 @@ const Tips: React.FC = () => {
         ></iframe>
       </div>
 
+      <div className="lecco-info" style={{ marginTop: '30px', textAlign: 'justify', padding: '20px', backgroundColor: '#f8f9fa', borderRadius: '8px' }}>
+        <h3 style={{ fontSize: '1.5em', marginBottom: '10px', color: '#444', textAlign: 'center' }}>Hospedagem em Lecco</h3>
+        <p style={{ fontSize: '1.2em', lineHeight: '1.8', color: '#555' }}>
+          Uma dica valiosa é considerar a hospedagem na charmosa cidade de <strong>Lecco</strong>, localizada às margens do famoso Lago di Como. Além de ser um destino encantador por si só, Lecco oferece uma infraestrutura mais ampla e diversas opções de hospedagem, atendendo a diferentes perfis de viajantes. A cidade também garante fácil acesso a <strong>Merate</strong>, tornando-se uma escolha prática e aconchegante para a sua estadia.
+        </p>
+      </div>
+
       <br />
 
-      <p className="map-intro">
-        Seguem algumas opções de hotéis pela região:
-      </p>
+      <p className="map-intro">Seguem algumas opções de hotéis pela região:</p>
 
       <div className="hotel-list">
         {hotelData.map((hotel, index) => (
@@ -90,33 +94,6 @@ const Tips: React.FC = () => {
             link={hotel.link}
           />
         ))}
-      </div>
-
-      <div className="region-activities" style={{ marginTop: '40px', padding: '20px' }}>
-        <h2 style={{ fontSize: '2.2em', marginBottom: '20px', textAlign: 'center', color: '#333' }}>
-          O que fazer pela região
-        </h2>
-
-        <h3 style={{ fontSize: '1.5em', marginTop: '20px', marginBottom: '10px', color: '#444', textAlign: 'left' }}>
-          Explorar Como e suas maravilhas
-        </h3>
-        <p style={{ fontSize: '1.1em', lineHeight: '1.6', color: '#555', textAlign: 'justify', marginBottom: '15px' }}>
-          A cidade de Como é um destino encantador, conhecido por sua beleza natural e por suas villas históricas. Um dos passeios mais recomendados é o tour de barco pelo Lago de Como, que permite conhecer as maravilhas da região de uma perspectiva única. Não perca a chance de visitar a <strong style={{ color: '#333' }}>Villa del Balbianello</strong>, uma impressionante villa histórica onde foram gravadas cenas de filmes como Star Wars e 007 - Casino Royale.
-        </p>
-
-        <h3 style={{ fontSize: '1.5em', marginTop: '20px', marginBottom: '10px', color: '#444', textAlign: 'left' }}>
-          Visite Bellagio, a pérola do Lago de Como
-        </h3>
-        <p style={{ fontSize: '1.1em', lineHeight: '1.6', color: '#555', textAlign: 'justify', marginBottom: '15px' }}>
-          Bellagio é uma charmosa cidade situada bem no centro do Lago de Como, entre as duas "pernas" do lago, oferecendo paisagens de tirar o fôlego. Além das belas vistas, você encontrará jardins deslumbrantes, ruas estreitas e uma atmosfera romântica que tornam Bellagio um lugar imperdível para quem visita a região.
-        </p>
-
-        <h3 style={{ fontSize: '1.5em', marginTop: '20px', marginBottom: '10px', color: '#444', textAlign: 'left' }}>
-          Brunate e sua vista panorâmica
-        </h3>
-        <p style={{ fontSize: '1.1em', lineHeight: '1.6', color: '#555', textAlign: 'justify', marginBottom: '15px' }}>
-          Para uma vista espetacular da região, visite Brunate, uma pequena vila localizada no topo de uma montanha ao lado de Como. Você pode chegar a Brunate através de um funicular que parte de Como, e ao chegar, aproveite a caminhada até o <strong style={{ color: '#333' }}>Farol de Brunate</strong>. Do alto do farol, você terá uma vista incrível de toda a região e do Lago de Como, proporcionando uma experiência memorável.
-        </p>
       </div>
     </div>
   );
